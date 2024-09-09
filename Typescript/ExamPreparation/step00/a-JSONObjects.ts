32382e9943116e181730994d67aa5a06c65171ca
ToobaTaqi <toobtq@gmail.com>
2024-09-10 02:20:34 +0500

    milestone1

---
+++ Typescript/ExamPreparation/step00/a-JSONObjects.ts
@@ -0,0 +1,11 @@
+// JSON syntax
+
+const jesonObj = {
+  name: "Vipin",
+
+  age: 21,
+
+  gender: "male",
+};
+
+console.log(jesonObj['name'])
---
+++ Typescript/ExamPreparation/step00/d-AssignabilityError.ts
@@ -0,0 +1,3 @@
+let message = "Hello World";
+message = 6; //can reassign with the same data type but if we reassign a variable with a different data type, it's assignability error
+console.log(message);
