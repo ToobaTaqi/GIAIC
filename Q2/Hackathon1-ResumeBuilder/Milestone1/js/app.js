43237a3a0716f03d89e47e71ce358d967516460c
ToobaTaqi <toobtq@gmail.com>
2024-09-10 11:23:46 +0500

    Milestone1

--- Q2/Hackathon1-ResumeBuilder/Milestone1/js/app.js
+++ Q2/Hackathon1-ResumeBuilder/Milestone1/js/app.js
@@ -1,7 +1,6 @@
 function handleSave() {
     alert("saved");
 }
-// Attaching event listeners after the DOM content is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll("button");
     buttons.forEach(button => {
