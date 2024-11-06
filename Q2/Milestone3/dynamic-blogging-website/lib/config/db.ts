f5bfde0c8799cd0c4ccda22dc666717949eee80d
ToobaTaqi <toobtq@gmail.com>
2024-11-06 15:27:19 +0500

    blog

--- Q2/Milestone3/dynamic-blogging-website/lib/config/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://user1:Sz1KHpALpaeeOSsS@cluster0.gevcxf5.mongodb.net/blogs"
-  );
-  console.log("DB Connected");
-};
