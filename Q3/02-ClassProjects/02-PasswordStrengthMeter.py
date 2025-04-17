27a816f487df1aeaf06822cc912e2bdbc8a54883
ToobaTaqi <toobtq@gmail.com>
2025-04-17 18:59:18 +0500

    02Done

--- Q3/02-ClassProjects/02-PasswordStrengthMeter.py
+++ Q3/02-ClassProjects/02-PasswordStrengthMeter.py
@@ -6,37 +6,40 @@ def check_password_strength(password):
     score = 0
     
     # Length Check
-    if len(password) >= 8:
-        score += 1
-    else:
-        st.write("❌ Password should be at least 8 characters long.")
+    if len(password) == 0:
+        st.write(":red[Please Enter Pawword!]")
+    else:  
+        if len(password) >= 8:
+            score += 1
+        else:
+            st.write("❌ Password should be at least 8 characters long.")
     
-    # Upper & Lowercase Check
-    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
-        score += 1
-    else:
-        st.write("❌ Include both uppercase and lowercase letters.")
+        # Upper & Lowercase Check
+        if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+            score += 1
+        else:
+            st.write("❌ Include both uppercase and lowercase letters.")
     
-    # Digit Check
-    if re.search(r"\d", password):
-        score += 1
-    else:
-        st.write("❌ Add at least one number (0-9).")
+        # Digit Check
+        if re.search(r"\d", password):
+            score += 1
+        else:
+            st.write("❌ Add at least one number (0-9).")
     
-    # Special Character Check
-    if re.search(r"[!@#$%^&*]", password):
-        score += 1
-    else:
-        st.write("❌ Include at least one special character (!@#$%^&*).")
+        # Special Character Check
+        if re.search(r"[!@#$%^&*]", password):
+            score += 1
+        else:
+            st.write("❌ Include at least one special character (!@#$%^&*).")
     
-    # Strength Rating
-    if score == 4:
-        st.write(":green[✅ Strong Password!]")
-    elif score == 3:
-        st.write(":blue[⚠️ Good but missing some security features.]")
-        # st.write(":yello[⚠️ Moderate Password - Consider adding more security features.]")
-    else:
-        st.write(":red[❌ Weak Password - Improve it using the suggestions above.]")
+        # Strength Rating
+        if score == 4:
+            st.write(":green[✅ Strong Password!]")
+        elif score == 3:
+            st.write(":blue[⚠️ Good but missing some security features.]")
+            # st.write(":yello[⚠️ Moderate Password - Consider adding more security features.]")
+        else:
+            st.write(":red[❌ Weak Password - Improve it using the suggestions above.]")
 
 # Ui
 password = st.text_input("Enter your password")
---
+++ Q3/02-ClassProjects/Readme.md
@@ -0,0 +1,5 @@
+### 01 Unit Converter
+- https://01-unitconverterpy.streamlit.app/
+
+### 02 Password Strength Generator
+- 
